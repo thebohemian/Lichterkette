1ca868b6fdc49a347536d06464a2a70fcea81b76
Robert Schuster <thebohemian@gmx.net>
2016-06-08 20:06:30 +0200

    * made server more configurable

--- server/main.js
+++ server/main.js
@@ -2,18 +2,25 @@ var Web3 = require('web3');
 var fs = require('fs');
 
 var config = JSON.parse(fs.readFileSync("./server/config.json", "utf8"));
+var profile = config[config.profile];
 
-var sandboxId = config.sandboxId;
-var hostname = config.hostname;
+var url;
+switch (profile.provider) {
+  case 'nodeUrl':
+    url = profile.nodeUrl;
+    break;
+  case 'sandbox':
+  default:
+    url = 'https://' + profile.hostname + ':8555/sandbox/' + profile.sandboxId;
+    break;
+}
 
-var url = 'https://' + hostname + ':8555/sandbox/' + sandboxId;
 var web3 = new Web3(new Web3.providers.HttpProvider(url));
-
-web3.eth.defaultAccount = '0xdedb49385ad5b94a16f236a6890cf9e0b1e30392';
+web3.eth.defaultAccount = profile.defaultAccount;
 
 //  var c = contracts['LichterkettenMasterKontrollProgramm'].contract;
 var abi = [{"constant":false,"inputs":[{"name":"b","type":"address"}],"name":"setBeneficiary","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"},{"name":"y","type":"uint256"},{"name":"z","type":"uint256"}],"name":"operate","outputs":[],"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"who","type":"address"},{"indexed":false,"name":"x","type":"uint256"},{"indexed":false,"name":"y","type":"uint256"},{"indexed":false,"name":"z","type":"uint256"}],"name":"Operated","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"remainingTime","type":"uint256"}],"name":"Busy","type":"event"}];
-var c = web3.eth.contract(abi).at('0x17956ba5f4291844bc25aedb27e69bc11b5bda39');
+var c = web3.eth.contract(abi).at(profile.contractAddress);
 
 var setLight = function(r,g,b) {
     console.log('Color set to: ' + r + " - " + g + " - " + b);
